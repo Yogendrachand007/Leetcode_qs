c628fdea8fccfe27254a7b77a67df57cd30cf946
Yogendra Chand <76557778+Yogendrachand007@users.noreply.github.com>
2023-02-24 23:29:15 +0530

    Time: 11 ms (88.16%), Space: 8.8 MB (78.51%) - LeetHub

---
+++ 0383-ransom-note/0383-ransom-note.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) {
+        
+       int counter[26] = {0};
+        
+        for(char ch : magazine)
+        {
+            counter[ch - 'a']++;
+        }
+        for(char ch : ransomNote)
+        {
+            if(counter[ch - 'a']-- <= 0)
+                return false;
+        }
+        return true;
+        
+        
+        
+    }
+};
