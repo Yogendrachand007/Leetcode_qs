fe9a0f51cc4f917608a744bdd9b11cd20efea03d
Yogendra Chand <76557778+Yogendrachand007@users.noreply.github.com>
2023-03-12 23:34:01 +0530

    Time: 19 ms (16.45%), Space: 9.9 MB (97.23%) - LeetHub

--- 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.cpp
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.cpp
@@ -4,22 +4,22 @@ public:
         
         sort(nums1.begin(),nums1.end());
         sort(nums2.begin(),nums2.end());
-        int m = nums1.size();
-        int n = nums2.size();
 
-        vector<int> ans;
-        for(int i=0;i<m;i++){
-            if(i>0&& nums1[i-1]==nums1[i])
-             continue;
-            for(int j=0;j<n;j++){
+        vector<int>ans;
+      
+        for(int i=0;i<nums1.size();i++)
+        {
+            if(i>0 && nums1[i-1]==nums1[i])
+                continue;
+            for(int j=0;j<nums2.size();j++)
+            {
                 if(nums1[i]==nums2[j]){
                     ans.push_back(nums1[i]);
-                    break;
-                }
+                 break;
             }
-        } 
-
-        return ans;
+            }
+        }
+  return ans;
     }
 };
 
