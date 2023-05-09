6732c6cc26b318efc535ca13c4c49aa2235b2a9f
Yogendra Chand <76557778+Yogendrachand007@users.noreply.github.com>
2023-05-09 23:30:01 +0530

    Time: 0 ms (100.00%), Space: 6.5 MB (56.00%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -2,15 +2,26 @@ class Solution {
 public:
     int uniquePaths(int m, int n) {
      
-        int N = m+n-2;  // total no of steps
-        int r = m-1;  // arrangements.. even n-1 doesn't matter 
-        long res = 1;
+//         int N = m+n-2;  // total no of steps
+//         int r = m-1;  // arrangements.. even n-1 doesn't matter 
+//         long res = 1;
         
-        for(int i=1;i<=r;i++)
+//         for(int i=1;i<=r;i++)
+//         {
+//             res = res* (N-r+i)/i;    
+//         }
+//         return res;
+        
+        vector<vector<int>>dp(m, vector<int>(n,1));
+        
+        for(int i=1;i<m;i++)
         {
-            res = res* (N-r+i)/i;    
+            for(int j=1;j<n;j++)
+            {
+                dp[i][j] = dp[i-1][j]+dp[i][j-1];
+            }
         }
-        return res;
+        return dp[m-1][n-1];
         
     }
 };
