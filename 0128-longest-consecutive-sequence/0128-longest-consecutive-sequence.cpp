078206be7ee604464a0c21defc1eb1bec8556c1d
Yogendra Chand <76557778+Yogendrachand007@users.noreply.github.com>
2023-05-29 23:07:11 +0530

    Time: 2209 ms (5.12%), Space: 68.8 MB (7.68%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
@@ -11,12 +11,12 @@ public:
         
         for(int i=0;i<nums.size();i++)
         {
-            int curr_ele = nums[i];
-            int prev_ele = curr_ele - 1;
+            int curr_ele = nums[i];   // if 99 
+            int prev_ele = curr_ele - 1;  // so 99-1 = 98
             int count=0;
             if(s.find(prev_ele)==s.end()) // means prev ele NOT found from 1st till end then....
             {
-                while(s.find(curr_ele)!=s.end())  // not found prev ele,  so start a chain with that curr ele
+                while(s.find(curr_ele)!=s.end())  // not found prev ele,  so start a chain with that curr ele// now find the next element if persent then curr_ele ++ and count++ then continue till  no more next ele 
                 {
                     curr_ele++;
                     count++;
