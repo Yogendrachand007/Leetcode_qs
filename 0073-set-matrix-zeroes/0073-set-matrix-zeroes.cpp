21a6bb8a640a75dca8ab070b451c2e9fb2bf7351
Yogendra Chand <76557778+Yogendrachand007@users.noreply.github.com>
2023-05-31 22:47:24 +0530

    Time: 22 ms (14.39%), Space: 13.6 MB (9.15%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -4,19 +4,12 @@ public:
         vector<vector<int>>ans = matrix;
          int m = matrix.size();
         int n=matrix[0].size();
-        
-       
-       
-        for(int i=0;i<m;i++)
-        {
-            for(int j=0;j<n;j++)
-            {
-            if(matrix[i][j]==0)
-               {
+         for(int i=0;i<m;i++) {
+            for(int j=0;j<n;j++){
+            if(matrix[i][j]==0){
                 for(int k=0;k<n;k++)
                 {
-                ans[i][k]=0;  // Left and right column to be 0
-                }
+                ans[i][k]=0; }  // Left and right column to be 0 
                }
             }
         }
@@ -33,10 +26,8 @@ public:
               }
              }
         }
-         for(int i=0;i<m;i++)
-        {
-            for(int j=0;j<n;j++)
-            {
+         for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
                 matrix[i][j]=ans[i][j];
             }
             }  
